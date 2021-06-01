76a50b220b356be2a84ec8eb604efc156345cdc1
Jhonatan Oliveira <Jhonatandev0@gmail.com>
2021-06-01 20:33:04 -0300

    Corrigindo o commit anterior

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -27,23 +27,11 @@
         </div>
       </el-popover>
     </template>
-    <template slot="navbar-menu">
-      <drop-down
-        tag="li"
-        title="Acessar Conta"
-        icon="now-ui-icons gestures_tap-01"
-        class="nav-item"
-      >
-        <nav-link to="/profile">
-          <i class="now-ui-icons users_single-02"></i> Profile
-        </nav-link>
-      </drop-down>
-    </template>
   </navbar>
 </template>
 
 <script>
-import { DropDown, Navbar, NavLink } from "@/components";
+import { Navbar } from "@/components";
 import { Popover } from "element-ui";
 export default {
   name: "main-navbar",
@@ -52,9 +40,7 @@ export default {
     colorOnScroll: Number,
   },
   components: {
-    DropDown,
     Navbar,
-    NavLink,
     [Popover.name]: Popover,
   },
 };
